65f5769bc8eafca3c3466ef76fcccf1f01bb37ba
Maycol Michel Mohr <maycolmohr@yahoo.com.br>
2021-03-24 18:57:52 +0100

    switching player each turn

--- src/main/java/application/UI.java
+++ src/main/java/application/UI.java
@@ -5,6 +5,7 @@
  */
 package application;
 
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -60,6 +61,14 @@ public class UI {
         }
     }
     
+    public static void printMatch(ChessMatch chessMatch) {
+        printBoard(chessMatch.getPieces());
+        System.out.println("");
+        System.out.println("Turn: " + chessMatch.getTurn());
+        System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+        
+    } 
+    
     
     public static void printBoard(ChessPiece[][] pieces) {
        for(int i=0; i< pieces.length; i++) {
@@ -72,6 +81,7 @@ public class UI {
        
            System.out.println("  a b c d e f g h");
     }
+    
     
     
     public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
