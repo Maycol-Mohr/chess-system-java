067975daafec22cad9e2aa07f769099b98ca86a0
Maycol Michel Mohr <maycolmohr@yahoo.com.br>
2021-03-25 18:59:10 +0100

    Knight

---
+++ src/main/java/chess/pieces/Knight.java
@@ -0,0 +1,101 @@
+ /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+/**
+ *
+ * @author marin
+ */
+public class Knight extends ChessPiece{
+    
+    public Knight(Board board, Color color) {
+        super(board, color);
+    }
+    
+    @Override
+    public String toString() {
+        return "N";
+    } 
+    
+    private boolean canMove(Position position) {
+       ChessPiece p = (ChessPiece)getBoard().piece(position);
+       return p == null || p.getColor()!= getColor();
+    }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()]; 
+        
+        Position p = new Position(0, 0);
+        
+        //above
+        p.setValues(position.getRow() - 1, position.getColumn() - 2);
+        if(getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true; 
+            
+        }
+        
+        //below
+        p.setValues(position.getRow() - 2, position.getColumn() - 1);
+        if(getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true; 
+            
+        }
+        
+        //left
+        p.setValues(position.getRow() - 2, position.getColumn() + 1);
+        if(getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true; 
+            
+        }
+        
+        //right
+        p.setValues(position.getRow() -1, position.getColumn() + 2);
+        if(getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true; 
+            
+        }
+        
+        //nw
+        p.setValues(position.getRow() + 1, position.getColumn() + 2);
+        if(getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true; 
+            
+        }
+        
+        //ne
+        p.setValues(position.getRow() + 2, position.getColumn() + 1);
+        if(getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true; 
+            
+        }
+        
+        //sw
+        p.setValues(position.getRow() + 2, position.getColumn() - 1);
+        if(getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true; 
+            
+        }
+        
+        //se
+        p.setValues(position.getRow() + 1, position.getColumn() - 2);
+        if(getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getColumn()] = true; 
+            
+        }
+        
+        
+        return mat;    
+    }
+    
+}
+
